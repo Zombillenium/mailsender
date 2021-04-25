ca881660e45304d01777b3c04fc5a79667b09016
Zombillenium <71927954+Zombillenium@users.noreply.github.com>
2021-04-25 14:21:11 +0200

    Update simplemailsender.py

--- simplemailsender.py
+++ simplemailsender.py
@@ -7,15 +7,6 @@
 # Copyright:   (c) Léo fumet 2021
 # All rights reserved
 #-------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-# Name:        e-mail writer
-#
-# Author:      Léo
-#
-# Created:     05/04/2021
-# Copyright:   (c) Léo fumet 2021
-# All rights reserved
-#-------------------------------------------------------------------------------
 lienid= "https://accounts.google.com/AccountChooser/identifier?service=mail&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&flowName=GlifWebSignIn&flowEntry=AccountChooser"
 n = 0
 
@@ -23,7 +14,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-Idgoogle = ""
+# Merci de replir ce champs de caractère :
+Idgoogle = "" # Merci d'utiliser un mail professionnel, au quel cas une erreur de sécurité apparaitra
 Mdpgoogle = ""
 destinataire = ["destinataire@gmail.com", "destinataire@Waouh.org", "destinataire@ecosia.fr"]
 objet = "objet du message"
@@ -53,9 +45,11 @@ while n!= len(destinataire) :
     nouveaumessage_search.click()
 
     #écrire le message
+    time.sleep(1)
     ecriredestinataire_search = driver.find_element_by_class_name("vO")
     ecriredestinataire_search.send_keys(destinataire[n])
     ecrireobjet_search = driver.find_element_by_class_name("aoT")
+    time.sleep(0.5)
     ecrireobjet_search.send_keys(objet)
     ecriremessage_search = driver.find_element_by_class_name("gmail_signature")
     ecriremessage_search.click()
